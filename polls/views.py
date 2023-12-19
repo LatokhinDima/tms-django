7e7a4003fa21c5c1102daa7b19d7385b9920b0b2
LatokhinDima <latokhindima@gmail.com>
2023-12-19 22:00:37 +0300

    1

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest
 
-def index(request):
-    return HttpResponse('Hello, Django! This is polls app.')
+from .models import Question
+
+
+def index(request: HttpRequest):
+    question = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': question}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id: int):
+    try:
+        question = Question.objects.get(id=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
