58d78a8fab3358bc813683e44d748c98261d845a
LatokhinDima <latokhindima@gmail.com>
2023-12-24 17:28:27 +0300

    2

---
+++ database_utils.py
@@ -0,0 +1,20 @@
+import os
+import json
+from polls.models import Question, Choice
+
+
+def populate_polls_database(file_json: str, clean_database: bool = True):
+    if clean_database:
+        Question.objects.all().delete()
+        Choice.objects.all().delete()
+
+    with open(file_json, 'r') as file:
+        data = json.load(file)
+
+    for question_text, choices_data in data.items():
+        question = Question.objects.create(question_text=question_text)
+
+        for choice_text, votes in choices_data.items():
+            Choice.objects.create(question=question, choice_text=choice_text, votes=votes)
+
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls',
+    'articles',
+    'shop',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
