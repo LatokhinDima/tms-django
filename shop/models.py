a7e84b670d1d1d0e37bf4a20fb5e3943fbabb7ba
LatokhinDima <latokhindima@gmail.com>
2024-01-16 22:00:13 +0300

    2

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-
 
     def __str__(self):
         return self.name
@@ -16,3 +18,28 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Profile(models.Model):
+   user = models.OneToOneField(User, on_delete=models.CASCADE)
+   shopping_cart = models.OneToOneField('Order', on_delete=models.SET_NULL,
+                                       null=True, blank=True, related_name='+')
+
+   def __str__(self):
+        return self.user
+
+class OrderEntry(models.Model):
+    product = models.ForeignKey(Product, related_name='product', on_delete=models.CASCADE)
+    count = models.IntegerField(default=0)
+    order = models.ForeignKey('Order', related_name='order_entries', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product
+
+
+class Order(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, default='INITIAL')
+
+
+
+
