c09dc57a9f575328af2c65915df4994aad8fadf0
LatokhinDima <latokhindima@gmail.com>
2024-01-23 01:22:02 +0300

    2

--- shop/admin.py
+++ shop/admin.py
@@ -1,19 +1,25 @@
 from django.contrib import admin
-from .models import Category, Product, Profile, Order, OrderEntry
+from .models import Category, Product
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price', 'category']
+    search_fields = ['name']
+    list_filter = ['name', 'price', 'category']
+
+admin.site.register(Product, ProductAdmin)
+
 
 class ProductInline(admin.TabularInline):
     model = Product
     extra = 1
+    fields = ['name', 'description', 'price']
+
 
 class CategoryAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ['name']})
-    ]
-
+    list_display = ['name']
+    list_filter = ['name']
     inlines = [ProductInline]
 
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Profile)
-admin.site.register(Order)
-admin.site.register(OrderEntry)
+admin.site.register(Category, CategoryAdmin)
+
+
