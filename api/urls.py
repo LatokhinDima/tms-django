510ed57349e15ce21b5e0ca4f3bc842914135ee0
LatokhinDima <latokhindima@gmail.com>
2024-03-01 09:28:04 +0300

    0103

--- api/urls.py
+++ api/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('questions/<int:question_id>/vote/', views.choice_vote),
     path('', views.QuestionViewSet.as_view({'get': 'list'}), name="index"),
     path('users/register/', views.UserCreateView.as_view(), name="register"),
     path('users/register/<int:pk>/', views.UserCreateView.as_view(), name="update_user"),
